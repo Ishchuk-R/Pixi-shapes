c4d1d64d7d20324ac1373848712d6f183a766e00
Tetiana <ishchuktr@gmail.com>
2020-06-23 09:14:03 +0300

    solution

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import {PixiShapesController} from './controller/'
+import {PixiShapesModel} from './model/'
+import {PixiShapesView} from './view/'
+
+const model = new PixiShapesModel();
+const view  = new PixiShapesView(model);
+const controller = new PixiShapesController(view, model);
+
+controller.startGame();
